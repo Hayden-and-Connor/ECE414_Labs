bbb46fc24b37ff9d07f2995968f3cf8c24ce226d
Hayden Dodge (pc) <dodgeh@lafayette.edu>
2018-09-20 02:03:20 -0400

    Updated calculator to use static variables. Updated operator press behavior based on editting state.

--- Utilities/calculator.c
+++ Utilities/calculator.c
@@ -3,10 +3,22 @@
 
 #include <stdlib>
 
+// OPERATOR MACROS
 #define ADD '+'
 #define SUBTRACT '-'
 #define MULTIPLY '*'
 #define DIVIDE '/'
+#define EQUAL '='
+
+// STATE MACROS
+// least significant bit determines which register to edit
+// 2nd least significant bit determines whether or not to clear 
+// parameter when digit is pressed depending on if an operator was 
+// last pressed
+#define EDIT_PARAM_0 0
+#define EDIT_PARAM_1 1
+#define OVERRIDE_PARAM_0 2
+#define OVERRIDE_PARAM_1 3
 
 // min signed int:       - 2 , 1 4 7 , 4 8 3 , 6 4 7 \0
 // index of char in hex: 0 1 2 3 4 5 6 7 8 9 a b c d e
@@ -14,17 +26,23 @@
 
 static int param[2]; 
 static char operator;
+//static disp
 static char display[12];
+static int state;
 
 typedef struct{
 	char* display;
+	int* state;
 } calculator;
 
 calculator CALCULATOR = {
 	&display;
-}
+	&state;
+};
 
 void init_calculator(){
+	state = 0;
+
 	param[0] = 0;
 	param[1] = 0;
 	operator = '\0';
@@ -34,6 +52,7 @@ void init_calculator(){
 	}
 }
 
+// TODO negative overflow
 void add_digit(int param_index, int digit){
 	// check for overflow
 	if(param[param_index] > 0){
@@ -49,19 +68,51 @@ void add_digit(int param_index, int digit){
 	sprint(display, "%d", param[param_index]); // update display str in mem
 }
 
+void clear_param(int param_index){
+	param[param_index] = 0;
+}
+
 void digit_pressed(int digit){
-	if(operator == '\0'){
-		add_digit(0, digit);
-	} else {
+	if(state & 2){
+		// override state - clear
+		clear_param(state & 1);
+	} 
+	if(state & 1){
 		add_digit(1, digit);
+	} else {
+		add_digit(0, digit);
 	}
 }
 
-// void operator_pressed(char operator){
-// 	if(param[1] !=){
+// performs calcuation with current operator and stores in param[0]
+void calculate(){
+	if(c->operator == ADD){
+		param[0] += param[1];
+	} else if(c->operator == SUBTRACT){
+		param[0] -= param[1];
+	} else if(c->operator == MULTIPLY){
+		param[0] *= param[1];
+	} else if(c->operator == DIVIDE){
+		param[0] /= param[1];
+	}
+}
 
-// 	}
-// }
+void operator_pressed(char op){
+	if(op == EQUAL){
+		calculate(); // result stored in param[0] and display ptr is updated
+		state = OVERRIDE_PARAM_0;
+	} else {
+		operator = op;
+		if(state & 1){
+			calculate();
+			state = OVERRIDE_PARAM_1;
+		} else {
+			param[1] = param[0];
+			state = OVERRIDE_PARAM_1;
+		}
+	}
+	sprintf(display, "%d", param[0]); 
+}
 
 // void clear_entry(){
 
@@ -83,21 +134,7 @@ void digit_pressed(int digit){
 // 	c->operator = operator;
 // }
 
-// int calculate(){
-// 	if(c->param[0] == NULL || c->param[1] == NULL){
-// 		return NULL;
-// 	}
-// 	if(c->operator == ADD){
-// 		return c->param[0] + c->param[1];
-// 	} else if(c->operator == SUBTRACT){
-// 		return c->param[0] - c->param[1];
-// 	} else if(c->operator == MULTIPLY){
-// 		return c->param[0] * c->param[1];
-// 	} else if(c->operator == DIVIDE){
-// 		return c->param[0] / c->param[1];
-// 	}
-// 	return NULL;
-// }
+
 
 // void clear_param(int param_index){
 // 	c->param[param_index] = NULL;
