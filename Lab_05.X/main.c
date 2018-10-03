cf1edb994ad9eccedb8ff731e818491408b30985
Hayden Dodge <dodgeh@ad.lafayette.edu>
2018-10-03 18:45:05 -0400

    Fixed merge that was still in main.c

--- Lab_05.X/main.c
+++ Lab_05.X/main.c
@@ -9,8 +9,8 @@
 #pragma config FPBDIV = DIV_1, FPLLODIV = DIV_2 // PB 40 MHz
 #pragma config FWDTEN = OFF,  FSOSCEN = OFF, JTAGEN = OFF
 
-<<<<<<< HEAD
 char write_buffer[64];
+
 void on_uart_value(char* value){
 	// sprintf(write_buffer, "test");
 	UART.write_string("test");
@@ -35,30 +35,19 @@ void main(){
 
 	// sprintf(write_buffer, "hello %d", test_event -> size);
 	// UART.write_string(write_buffer);
-=======
-uint8_t buffer[64];
 
-void main(){
-	UART.init();
-	int i;
+// // working analog read
+// 	UART.init();
+// 	int i;
 
-	analog_in_init();
-	int32_t volt = 0;
-    
-    int32_t count = 0;
-	
+// 	analog_in_init();
+// 	int32_t volt = 0;
+// 	while(1){
+// 		// volt = analog_in_read(9);
+// 		// sprintf(buffer, "%04d    \r \n");
+// 		// UART.write_string(buffer);
+// 		// for(i = 0; i < 1000000; i++){
 
-	while(1){
-		// encoder read
-		
-
-		// // working analog read
-		// volt = analog_in_read(9);
-		// sprintf(buffer, "%04d    \r \n");
-		// UART.write_string(buffer);
-		// for(i = 0; i < 1000000; i++){
-
-		// }
-	}
->>>>>>> 7214b857bf24133974aca975d7d640c9fe269da0
-}
+// 		// }
+// 	}
+// }
